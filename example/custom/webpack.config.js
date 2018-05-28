19deeadc90e521e2ab3611ae23c5fff0fff69b21
Samuel R <desconocido_-_93@hotmail.com>
2018-05-28 12:52:54 -0500

    Add custom example for new async hook (webappWebpackPluginBeforeEmit)
    - Delete callback example
    - Add custom example in webpack.config.js (plugins)

--- example/custom/webpack.config.js
+++ example/custom/webpack.config.js
@@ -29,48 +29,6 @@ module.exports = (env, args) => {
         // Inject html links/metadata (requires html-webpack-plugin)
         inject: true,
         // Favicons configuration options. Read more on: https://github.com/evilebottnawi/favicons#usage
-        alterFaviconsEmit: (result, options) => {
-
-          result.html = result.html.split('>');
-
-          result.html = result.html
-            .filter(html => html.length > 0)
-            .map(html => {
-              // Repait split
-              html += '>';
-              // Alter meta theme color, change from background color to theme color
-              html = html == `<meta name="theme-color" content="${options.background}">` ? `<meta name="theme-color" content="${options.theme_color}">` : html;
-              // Alter meta msapplication tile color, change from background color to theme color
-              html = html == `<meta name="msapplication-TileColor" content="${options.background}">` ? `<meta name="msapplication-TileColor" content="${options.theme_color}">` : html;
-              // Alter manifest, from manifest.json to manifest.webmanifest, according to example in: https://www.w3.org/TR/appmanifest/#using-a-link-element-to-link-to-a-manifest
-              html = (/(rel\=.{1}manifest.{1}.*)/.test(html)) ? html.replace('manifest.json', 'manifest.webmanifest') : html;
-              return html;
-            })
-            .reduce((prev, next) => {
-              // Convert to string
-              return `${prev}${next}`
-            });
-
-          // Alter manifest.json
-          result.assets.forEach(file => {
-            if (/manifest\.json$/.test(file.name) && !(/(yandex)/.test(file.name))) {
-              // Change name of manifest.json to manifest.webmanifest, according to example in: https://www.w3.org/TR/appmanifest/#using-a-link-element-to-link-to-a-manifest
-              file.name = file.name.replace('manifest.json', 'manifest.webmanifest');
-              const manifestJson = JSON.parse(file.contents);
-              // Add some extra options to manifest.webmanifest that favicons library does not return
-              // Result validated in: https://manifest-validator.appspot.com/
-              manifestJson.scope = options.scope;
-              manifestJson.short_name = options.appShortName;
-              manifestJson.developer = {
-                name: options.developerName,
-                url: options.developerURL,
-              };
-              manifestJson.version = options.version;
-              file.contents = JSON.stringify(manifestJson);
-            }
-          });
-          return result;
-        },
         favicons: {
           appName: 'My WebApp with WebApp Webpack Plugin',              // Your application's name. `string`
           appShortName: 'WebApp',                                         // Your application's short name. `string` : Not implemented in favicons library
@@ -107,6 +65,18 @@ module.exports = (env, args) => {
           }
         },
       }),
+      new class {
+        apply(compiler) {
+          compiler.hooks.make.tapAsync("A", (compilation, callback) => {
+            compilation.hooks.webappWebpackPluginBeforeEmit.tapAsync("B", (result, callback) => {
+              console.log(result);
+              return callback(null, result);
+            });
+
+            return callback();
+          })
+        }
+      }
     ],
     stats: "errors-only"
   };
