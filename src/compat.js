813b8cd5c43eec0d30a5312a0d2821823a6c6a06
Samuel R <desconocido_-_93@hotmail.com>
2018-05-29 16:58:22 -0500

    Make waterfall a helper in compat.js file
    Make waterfall a function that validates the Webpack version and responds to it, according to: https://github.com/brunocodutra/webapp-webpack-plugin/pull/90#discussion_r191569603
    TODO: Validate in webpack version < 4

--- src/compat.js
+++ src/compat.js
@@ -1,18 +1,29 @@
 const camelCase = require('camelcase');
 
 /* istanbul ignore next */
-module.exports.getAssetPath = ({mainTemplate}, name, args) => (
-    mainTemplate.getAssetPath /* Webpack >= 4.0 */
-  ? mainTemplate.getAssetPath(name, args)
-  : mainTemplate.applyPluginsWaterfall('asset-path', name, args)
+module.exports.getAssetPath = ({ mainTemplate }, name, args) => (
+  mainTemplate.getAssetPath /* Webpack >= 4.0 */
+    ? mainTemplate.getAssetPath(name, args)
+    : mainTemplate.applyPluginsWaterfall('asset-path', name, args)
 );
 
 /* istanbul ignore next */
 module.exports.tap = (tappable, hook, name, plugin) => (
-    tappable.hooks /* Webpack >= 4.0 */
-  ? tappable.hooks[camelCase(hook)] && tappable.hooks[camelCase(hook)].tapAsync(name, plugin)
-  : tappable.plugin(hook, plugin)
+  tappable.hooks /* Webpack >= 4.0 */
+    ? tappable.hooks[camelCase(hook)] && tappable.hooks[camelCase(hook)].tapAsync(name, plugin)
+    : tappable.plugin(hook, plugin)
 );
+
+/* istanbul ignore next */
+module.exports.waterfall = (tappable, hook, arg, callback) => {
+  if (tappable.hooks) { /* Webpack >= 4.0 */
+    tappable.hooks[camelCase(hook)].callAsync(arg, callback);
+  } else if (tappable.applyPluginsAsyncWaterfall) {
+    tappable.applyPluginsAsyncWaterfall(hook, arg, callback);
+  } else {
+    throw 'Validate plugin in Webpack < 4';
+  }
+};
 
 /* istanbul ignore next */
 module.exports.getContext = (loader) => (loader.options && loader.options.context) || loader.rootContext;
